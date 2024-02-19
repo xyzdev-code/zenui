a4fe1b3949a9e7498dcafa4d1b87aabcfbcb67d6
xyzdev-code <111351240+xyzdev-code@users.noreply.github.com>
2024-02-19 23:42:31 +0000

    changed package url

--- zenavi/go.mod
+++ zenavi/go.mod
@@ -1,3 +1,3 @@
-module routing
+module github.com/xyzdev-code/zenui/tree/master/zenavi
 
 go 1.21.6
---
+++ zenavi/types.go
@@ -0,0 +1,49 @@
+package zenavi
+type Result[T any] struct { 
+	val T 
+	err error 
+} 
+func (res *Result[T]) Ok() bool { 
+	return res.err == nil 
+} 
+func (res *Result[T]) Then(fn func()) *Result[T]{ 
+	if res.Ok() {
+		 fn() 
+	} 
+	return res 
+} 
+func (res *Result[T]) Or(alt T) T {
+	 if res.Ok() {
+		 return res.val 
+	} else {
+		 return alt 
+	} 
+} 
+func (res *Result[T]) Catch(fn func(err error)) *Result[T]{ 
+	if !res.Ok() {
+		 fn(res.err) 
+	} 
+	return res 
+} 
+func (res *Result[T]) Defualt(fn func()){ 
+	fn()
+} 
+func Ok[T any](val T) Result[T] { 
+	return Result[T]{ 
+		val: val, 
+	} 
+} 
+func Err[T any](err error) Result[T] { 
+	return Result[T]{
+		 err: err, 
+	} 
+} 
+func ToResult[T any](val T, err error) Result[T] { 
+	return Result[T]{ 
+		val: val, 
+		err: err, 
+	} 
+}
+func ToNormal[T any](value Result[T]) (T,error){
+	return value.val, value.err
+}
